c59c14d056a792b0adbc147d284b92886222c05e
suraj <suraj200981@yahoo.co.uk>
2022-12-31 16:13:38 +0000

    User-Agent is being changed randomly with each request.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
+const randomUseragent = require('random-useragent');
 
 const app = express();
 
@@ -21,54 +22,70 @@ app.get('/api', (req, res) =>{
     res.send(html);
 })
 
+let globalResponse;
 //this endpoint will scrape fighter profiles
 app.get('/api/fighter', (req,res) => {
 
-    //for now i will hard code what the req query params would be
-    let firstName = "conor"
-    let lastName = "mcgregor"
+let userAgent = "";
+userAgent = randomUseragent.getRandom();
 
+//set req headers to random user agent
+req.headers['user-agent'] = userAgent;
+req.headers['User-Agent'] = userAgent;
+
+
+    console.log(userAgent);
+    //for now i will hard code what the req query params would be
+    let firstName = "khabib"
+    let lastName = "nurmag"
+    //step 1 search for fighter to retreive fighter profile url
     let initalUrlToScrape = 'https://www.sherdog.com/stats/fightfinder?SearchTxt='+firstName+'+'+lastName+'&weight=&association=';
-    request(initalUrlToScrape, (error, response, html) => {
+    //add more headers to the request
+    request(initalUrlToScrape,{headers:{ 'User-Agent': req.headers['user-agent']}},(error, response, html) => {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(html);
-
-
+            
             const fighterNameFound = $('.new_table td a');
              enhancedProfileUrlFoundOnPage = $(fighterNameFound).attr('href')
 
+            //step 2 navigate to fighter profile url and scrape everything
+            let profileUrlToScrape = 'https://www.sherdog.com'+enhancedProfileUrlFoundOnPage
+            
+           console.log("RESPONSE:::::",response);
+           globalResponse=response;
+             request(profileUrlToScrape, {headers:{ 'User-Agent': req.headers['user-agent']}},(error1, response1, html2) => {
 
-
-            //step 2
-             let profileUrlToScrape = 'https://www.sherdog.com'+enhancedProfileUrlFoundOnPage
-            console.log(profileUrlToScrape)
-             request(profileUrlToScrape, (error1, response1, html2) => {
-                console.log(response1.statusCode)
                 if (!error1 && response.statusCode == 200) {
                     const $ = cheerio.load(html2);
-        
+                    
+                    //find element
                     const fullName = $('.fighter-title h1 .fn');
                     const nickName = $('.fighter-title h1 .nickname, .fighter-title h1 .nickname_empty');
-            
-                    const fullname1 = $(fullName).text();
-                    const nickName1 = $(nickName).text();
+                    const birthCountry = $('.fighter-title .birthplace strong');
+                    const fightingOutOf = $('span.locality');
 
-                    console.log(nickName1);
                     
+                    //scrape the data
+                    const fullnameValue = $(fullName).text();
+                    const nickNameValue = $(nickName).text();
+                    const birthCountryValue = $(birthCountry).text();
+                    const fightingOutOfValue = $(fightingOutOf).text();
+
                   //push data found to global array
                   data.push({
-                    name: fullname1,
-                    nickname: nickName1
+                    name: fullnameValue,
+                    nickname: nickNameValue,
+                    country: birthCountryValue,
+                    fightingOutOf: fightingOutOfValue,
+
                   })
-                
-                  res.send(data);
+                  res.send(globalResponse);
                 }
                 });
         }
         });   
 
-        });
-
+});
 
 
 
