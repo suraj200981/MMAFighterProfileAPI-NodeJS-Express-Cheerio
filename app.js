676140eb95b8a0c7ba6a1f34aab1111ab5fec09b
suraj <suraj200981@yahoo.co.uk>
2023-01-18 18:53:43 +0100

    will deploy later today

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fighterAnalysis = require("./routes/fighterAnalysis");
 
 //directs to routes
 app.use("/api", fightersRouter);
-app.use("/analysis", fighterAnalysis);
+// app.use("/analysis", fighterAnalysis);
 
 app.listen(8080, () => {
   console.log("MMA Fighter API started on http://localhost:8080/");
