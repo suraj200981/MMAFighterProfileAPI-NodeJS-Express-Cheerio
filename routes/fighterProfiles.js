63a2db4bed8ba60fcfbf0406f9af35531891fb56
suraj <suraj200981@yahoo.co.uk>
2023-01-19 19:18:41 +0100

    logging query params for heroku

--- routes/fighterProfiles.js
+++ routes/fighterProfiles.js
@@ -18,7 +18,7 @@ router.get("/fighter", async (req, res) => {
   let firstName = req.query.firstName;
   let lastName = req.query.lastName;
 
-  console.log("query params: " + firstname + lastname);
+  console.log("query params: " + firstName + lastName);
 
   // initialize a variable to keep track of whether the fighter name has been found
   let fighterNameFound = false;
