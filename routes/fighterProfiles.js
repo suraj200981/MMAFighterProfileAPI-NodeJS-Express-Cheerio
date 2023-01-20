96278929540f29f2c8d5f15476453cd2816bae39
suraj <suraj200981@yahoo.co.uk>
2023-01-20 22:09:26 +0100

    JWT bearer token protection for scrape all profiles and scrape new fighters endpoint Fixes #6

--- routes/fighterProfiles.js
+++ routes/fighterProfiles.js
@@ -14,57 +14,69 @@ let jwtSecret = crypto.randomBytes(64).toString("hex");
 
 // this endpoint will scrape fighter profiles
 router.get("/fighter", async (req, res) => {
-  // define a local variable to keep track of the current page number
-  let userAgent = "";
-  userAgent = randomUseragent.getRandom();
-  // set req headers to random user agent
-  req.headers["user-agent"] = userAgent;
+  let token = req.headers.authorization;
+  // console.log(token);
+  try {
+    const decoded = jsonwebtoken.verify(token, jwtSecret);
 
-  let firstName = req.query.firstName;
-  let lastName = req.query.lastName;
+    // define a local variable to keep track of the current page number
+    let userAgent = "";
+    userAgent = randomUseragent.getRandom();
+    // set req headers to random user agent
+    req.headers["user-agent"] = userAgent;
 
-  console.log("query params: " + firstName + lastName);
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
 
-  // initialize a variable to keep track of whether the fighter name has been found
-  let fighterNameFound = false;
+    console.log("query params: " + firstName + lastName);
 
-  // define a function to scrape the fighter blocks from a single page
-  function scrapePage(pageNumber) {
-    let url = `https://www.sherdog.com/stats/fightfinder?association=&weightclass=&SearchTxt=${firstName}+${lastName}&page=${pageNumber}`;
-    return findFighter.find(url, req);
-  }
+    // initialize a variable to keep track of whether the fighter name has been found
+    let fighterNameFound = false;
 
-  // use a loop to repeatedly scrape the next page until the fighter name is found
-  (async function loop() {
-    let pageNumber = 1; // define pageNumber here
-    while (!fighterNameFound) {
-      console.log(`Searching page ${pageNumber}...`);
-      try {
-        let figherBlocks = await scrapePage(pageNumber);
-        for (let i = 0; i < figherBlocks.length; i++) {
-          // if the fighter name matches the query params
-          if (
-            figherBlocks[i].name.toLowerCase() ==
-            (firstName + " " + lastName).toLowerCase()
-          ) {
-            // get the enhanced profile url
-            let enhancedProfileUrlFoundOnPage =
-              "https://www.sherdog.com" + figherBlocks[i].href;
-            console.log(`Found profile at ${enhancedProfileUrlFoundOnPage}`);
-            // set the flag to exit the loop
-            fighterNameFound = true;
-            // scrape the fighter's full profile
-            await scrapeRecord.scrape(enhancedProfileUrlFoundOnPage, res, req);
-            break;
-          }
-        }
-        // increment the page number
-        pageNumber++;
-      } catch (error) {
-        console.log(error);
-      }
+    // define a function to scrape the fighter blocks from a single page
+    function scrapePage(pageNumber) {
+      let url = `https://www.sherdog.com/stats/fightfinder?association=&weightclass=&SearchTxt=${firstName}+${lastName}&page=${pageNumber}`;
+      return findFighter.find(url, req);
     }
-  })();
+
+    // use a loop to repeatedly scrape the next page until the fighter name is found
+    (async function loop() {
+      let pageNumber = 1; // define pageNumber here
+      while (!fighterNameFound) {
+        console.log(`Searching page ${pageNumber}...`);
+        try {
+          let figherBlocks = await scrapePage(pageNumber);
+          for (let i = 0; i < figherBlocks.length; i++) {
+            // if the fighter name matches the query params
+            if (
+              figherBlocks[i].name.toLowerCase() ==
+              (firstName + " " + lastName).toLowerCase()
+            ) {
+              // get the enhanced profile url
+              let enhancedProfileUrlFoundOnPage =
+                "https://www.sherdog.com" + figherBlocks[i].href;
+              console.log(`Found profile at ${enhancedProfileUrlFoundOnPage}`);
+              // set the flag to exit the loop
+              fighterNameFound = true;
+              // scrape the fighter's full profile
+              await scrapeRecord.scrape(
+                enhancedProfileUrlFoundOnPage,
+                res,
+                req
+              );
+              break;
+            }
+          }
+          // increment the page number
+          pageNumber++;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    })();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 });
 
 router.get("/token", (req, res) => {
