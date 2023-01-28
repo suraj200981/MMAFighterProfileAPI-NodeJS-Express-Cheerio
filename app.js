6118b80ff5762e872a0cb8555ef90f8c5eb39eb5
suraj <suraj200981@yahoo.co.uk>
2023-01-28 22:57:49 +0100

    migrate JSON to DB progress Fixes #10

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const fightersRouter = require("./routes/fighterProfiles");
 const fighterAnalysis = require("./routes/fighterAnalysis");
 
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
 //directs to routes
 app.use("/api", fightersRouter);
 // app.use("/analysis", fighterAnalysis);
@@ -14,3 +17,20 @@ app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
   console.log("Server listening on Port", port);
 });
+
+//connect to mongoDB
+const client = new MongoClient(process.env.URI_MONGO);
+try {
+  client.connect();
+
+  listDatabases(client);
+} catch (e) {
+  console.error(e);
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
