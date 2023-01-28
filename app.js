c54a288a1f0b4f912a5324740833da56ef87de31
suraj <suraj200981@yahoo.co.uk>
2023-01-28 23:36:31 +0100

    successfully updating all profiles to mongodb on server start

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fightersRouter = require("./routes/fighterProfiles");
 const fighterAnalysis = require("./routes/fighterAnalysis");
 
 const { MongoClient } = require("mongodb");
+const fs = require("fs");
 require("dotenv").config();
 
 //directs to routes
@@ -13,13 +14,18 @@ app.use("/api", fightersRouter);
 
 let port = process.env.PORT || 8080; // live and local env port
 
+//read fighter profiles json
+const fighterProfiles = fs.readFileSync("FighterProfiles.json");
+
 app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
   console.log("Server listening on Port", port);
 });
 
 //connect to mongoDB
+
 const client = new MongoClient(process.env.URI_MONGO);
+
 client.connect((err) => {
   if (err) {
     console.error(err);
@@ -27,12 +33,25 @@ client.connect((err) => {
     listDatabases(client);
   }
 });
+updateProfilesOnServerStart(client, fighterProfiles);
 
 async function listDatabases(client) {
   try {
     databasesList = await client.db().admin().listDatabases();
     console.log("Databases:");
     databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//create function to update collection with json file on server start
+async function updateProfilesOnServerStart(client, fighterProfiles) {
+  try {
+    const db = client.db("FighterProfiles");
+    const collection = db.collection("FighterProfilesCollection");
+    const result = await collection.insertMany(JSON.parse(fighterProfiles));
+    console.log(result);
   } catch (e) {
     console.log(e);
   }
